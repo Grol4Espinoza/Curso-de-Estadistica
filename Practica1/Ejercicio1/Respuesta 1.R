74116f26ddad8f86f5e1618eab6df64c7669cbc0
Espinoza Mansilla <37357283+Grol4Espinoza@users.noreply.github.com>
2018-04-09 17:07:32 -0500

    Update Respuesta 1.R

--- Practica1/Ejercicio1/Respuesta 1.R
+++ Practica1/Ejercicio1/Respuesta 1.R
@@ -2,10 +2,18 @@
 
 #(a)
 x=seq(5,-11,by=-0.3)
+
 #(b)
 x=seq(-11,5,by=0.3)
+
 #(c)
 y=c(-1,3,-5,7,-9)
 y=rep(y,times=10)
 y=c(y,y)
 sort(y,decreasing = TRUE)
+
+#(d)
+
+z=c(seq(6,12),rep(5.3,3),-3,seq(102,100,by=-0.25))
+length(z)
+#efectivamente la longitud de z es 20
