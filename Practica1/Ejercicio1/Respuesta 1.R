fd568efcca2fa5c6ce59b44b9f88f9756fc4b823
Espinoza Mansilla <37357283+Grol4Espinoza@users.noreply.github.com>
2018-04-09 16:53:41 -0500

    Add files via upload

---
+++ Practica1/Ejercicio1/Respuesta 1.R
@@ -0,0 +1,11 @@
+# Nombre : Carlos Alberto Espinoza Mansilla 20162187C
+
+#(a)
+x=seq(5,-11,by=-0.3)
+#(b)
+x=seq(-11,5,by=0.3)
+#(c)
+y=c(-1,3,-5,7,-9)
+y=rep(y,times=10)
+y=c(y,y)
+sort(y,decreasing = TRUE)
